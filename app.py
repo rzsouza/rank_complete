bf25e424f66bef8e23eacb7ab174b72ae34a49bd
Rodrigo Ziviani de Souza <rodrigo.ziviani@gmail.com>
2023-08-09 11:28:32 -0300

    Extract sample data from main page

--- app.py
+++ app.py
@@ -5,7 +5,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.j2')
+    ranking = [
+        {'position': 1, 'name': 'Argentina', 'points': 6},
+        {'position': 2, 'name': 'France', 'points': 4},
+        {'position': 3, 'name': 'Brazil', 'points': 2},
+        {'position': 4, 'name': 'Germany', 'points': 1},
+    ]
+    return render_template('index.j2', ranking=ranking)
 
 
 if __name__ == '__main__':
