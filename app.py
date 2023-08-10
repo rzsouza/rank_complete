92f903e19c51111bc59dc15178e28cf2a6a23dbb
Rodrigo Ziviani de Souza <rodrigo.ziviani@gmail.com>
2023-08-10 19:36:30 -0300

    Bind match form to service

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -13,9 +13,19 @@ def index():
     ]
     return render_template('index.j2', ranking=ranking)
 
+
 @app.route('/admin')
 def admin():
     return render_template('admin.j2')
+
+
+@app.route('/match', methods=['POST'])
+def add_match():
+    home_team = request.form['home_team']
+    home_team_score = request.form['home_team_score']
+    away_team = request.form['away_team']
+    away_team_team_score = request.form['away_team_score']
+    return f"{home_team} {home_team_score} X {away_team} {away_team_team_score}"
 
 
 if __name__ == '__main__':
