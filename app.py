80c351d4a723af1af51585137ad1f252f20843b8
Rodrigo Ziviani de Souza <rodrigo.ziviani@gmail.com>
2023-08-10 19:40:12 -0300

    Lift state for ranking and matches.

--- app.py
+++ app.py
@@ -2,15 +2,18 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+ranking = [
+    {'position': 1, 'name': 'Argentina', 'points': 6},
+    {'position': 2, 'name': 'France', 'points': 4},
+    {'position': 3, 'name': 'Brazil', 'points': 2},
+    {'position': 4, 'name': 'Germany', 'points': 1},
+]
+
+matchs = []
+
 
 @app.route('/')
 def index():
-    ranking = [
-        {'position': 1, 'name': 'Argentina', 'points': 6},
-        {'position': 2, 'name': 'France', 'points': 4},
-        {'position': 3, 'name': 'Brazil', 'points': 2},
-        {'position': 4, 'name': 'Germany', 'points': 1},
-    ]
     return render_template('index.j2', ranking=ranking)
 
 
