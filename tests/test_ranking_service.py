45c8db26590f6ef6af3c0ec401db85133512378b
Rodrigo Ziviani de Souza <rodrigo.ziviani@gmail.com>
2023-08-18 11:19:34 -0300

    Fix tests

--- tests/test_ranking_service.py
+++ tests/test_ranking_service.py
@@ -14,8 +14,8 @@ class TestRankingService(TestCase):
         matches = [Match("Brazil", 0, "Argentina", 0)]
 
         expected_ranking = [
-            RankingStats("Argentina", 0, 1),
-            RankingStats("Brazil", 0, 1),
+            RankingStats("Argentina", 0, 0, 1),
+            RankingStats("Brazil", 0, 0, 1),
         ]
 
         self.assert_ranks_match_expected(expected_ranking, matches)
@@ -23,8 +23,8 @@ class TestRankingService(TestCase):
     def test_win_should_have_teams_with_3_and_0_points(self):
         matches = [Match("Italy", 2, "Germany", 0)]
         expected_ranking = [
-            RankingStats("Italy", 0, 3),
-            RankingStats("Germany", 0, 0),
+            RankingStats("Italy", 0, 0, 3),
+            RankingStats("Germany", 0, 0, 0),
         ]
 
         self.assert_ranks_match_expected(expected_ranking, matches)
@@ -36,9 +36,9 @@ class TestRankingService(TestCase):
         ]
 
         expected_ranking = [
-            RankingStats("Italy", 0, 4),
-            RankingStats("France", 0, 1),
-            RankingStats("Germany", 0, 0),
+            RankingStats("Italy", 0, 0, 4),
+            RankingStats("France", 0, 0, 1),
+            RankingStats("Germany", 0, 0, 0),
         ]
 
         self.assert_ranks_match_expected(expected_ranking, matches)
